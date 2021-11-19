06a8ed55d5ae564636eb6bef1be12e87ec6a635d
Maikoncarlos <maikonsaturno@gmail.com>
2021-11-18 21:33:02 -0300

    separando layout da tela login do sidbar e navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+
+  {
+    path: '', component: LayoutComponent,
+    children: [{ path: 'home', component: HomeComponent }],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
